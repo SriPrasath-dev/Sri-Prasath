51f9655dcbdf84efaa8974171a3abf88b0d4ded1
SriPrasath-dev <prasathsri2008@gmail.com>
2026-08-29 08:25:09 +0530

    Implement integer reversal function

---
+++ Reverse the integer.java
@@ -0,0 +1,30 @@
+class Solution {
+
+    public int reverse(int x) {
+
+        long rev = 0;
+
+        while (x != 0) {
+            int digit = x % 10;
+
+            rev = rev * 10 + digit;
+
+            x = x / 10;
+        }
+
+        if (rev > Integer.MAX_VALUE || rev < Integer.MIN_VALUE) {
+            return 0;
+        }
+
+        return (int) rev;
+    }
+
+    public static void main(String[] args) {
+
+        Solution obj = new Solution();
+
+        int x = 1534236469;
+
+        System.out.println(obj.reverse(x));
+    }
+}
